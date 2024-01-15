39117d67301b8679421cc422c1c3f30f1d986ac2
Caio Portugal <caiosantos.portugal@hotmail.com>
2024-01-15 16:43:37 -0300

    Adicionada a leitura dos campos da tela

--- AutomatizadorDeCadastros/AutomatizadorDeCadastros.cs
+++ AutomatizadorDeCadastros/AutomatizadorDeCadastros.cs
@@ -1,20 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AutomatizadorDeCadastros
 {
     public partial class AutomatizadorDeCadastros : Form
     {
+        public static string nomeDoDiretorio = "";
+        public static string nomeDoCadastro = "";
+
         public AutomatizadorDeCadastros()
         {
             InitializeComponent();
         }
+
+        private void botaobuscararquivo_Click(object sender, EventArgs e)
+        {
+            //verifica arquivo
+            OpenFileDialog itemParaCadastro = new OpenFileDialog();
+            DialogResult result = itemParaCadastro.ShowDialog();
+
+            textdiretoriodoarquivo.Text = itemParaCadastro.FileName;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (CamposEstaoPreenchidosCorretamente())
+            {
+                nomeDoCadastro = combotipocadastro.SelectedItem.ToString();
+                nomeDoDiretorio = textdiretoriodoarquivo.Text;
+            }
+
+
+            var nomeDoDiretorio3 = nomeDoDiretorio;
+            var nomeDoDiretorio4 = nomeDoCadastro;
+        }
+
+        private bool CamposEstaoPreenchidosCorretamente()
+        {
+            string mensagemDeErro = "";
+
+            if (textdiretoriodoarquivo.Text == "")
+            {
+                mensagemDeErro = "Preencha o diretório corretamente.";
+
+                MessageBox.Show(mensagemDeErro);
+
+                return false;
+            }
+
+            if (combotipocadastro.SelectedItem == null)
+            {
+                mensagemDeErro = "Selecione o Tipo de Cadastro corretamente.";
+
+                MessageBox.Show(mensagemDeErro);
+
+                return false;
+            }
+
+            return true;
+        }
     }
 }
+
+
+
+
