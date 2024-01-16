25800ed764a447a7ee711854e5acb5d203822c48
Caio Portugal <caiosantos.portugal@hotmail.com>
2024-01-15 22:30:53 -0300

    Obejtos adicionados

---
+++ AutomatizadorDeCadastros/Objetos/ArquivoDeCadastro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatizadorDeCadastros.Objetos
+{
+    public class ArquivoDeCadastro
+    {
+        public int Id { get; set; }
+        public string Cabecalho { get; set; }
+        public string TipoDeCadastro { get; set;}
+        public string[] Linhas { get; set; }
+    }
+}
---
+++ AutomatizadorDeCadastros/Objetos/Erro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutomatizadorDeCadastros.Objetos
+{
+    public class Erro
+    {
+        public static void ProcessarTelaDeErro(string mensagemdeerro)
+        {
+            MessageBox.Show(mensagemdeerro);
+        }
+    }
+}
